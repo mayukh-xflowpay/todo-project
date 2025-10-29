a0ba725107528f30ef83199a94190945ce433504
Mayukh Chaturvedi <mayukh@xflowpay.com>
2025-10-29 11:16:46 +0530

    deployment fix#1

--- frontend/app/todos/[todoID]/delete/page.tsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from "next/navigation";
-
-export default function DeleteTodo() {
-  const params = useParams();
-  const todoID = params.todoID as string;
-
-  return <h1>Delete todo page for {todoID}</h1>;
-}
